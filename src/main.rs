af568b104ad60dc0f25260248f5f2b2659c63ef2
Charis Karametos <charis.karametos@outlook.com>
2025-01-27 10:23:21 +0200

    added -v

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,39 @@ fn main() {
                 .long("input")
                 .value_name("FILE")
                 .help("Sets an input file")
-                .required(false),  // Make it optional
+                .required(false),  // Make it optiona
+        )
+        .arg(
+            Arg::new("version")
+                .short('v')
+                .long("Version")
+                .value_name("Version")
+                .help("Version")
+                .required(false)
         )
         .get_matches();
     let arg = matches.args_present();
     if !arg{
         println!("This is the cli tool for bugland, cuase im bored")
     } else {
-        let path = matches.get_one::<String>("input");
-        if let Some(path_str) = path {
-            let current_dir = env::current_dir().expect("Failed to get current directory");
-            let relative_path = current_dir.join(path_str); // `path_str` is a &String, which implements AsRef<Path>
-
-            if fs::metadata(&relative_path).is_ok() {
-                let interpreter =&mut Interpreter::new(); 
-                let contents = fs::read_to_string(path_str).expect("Failed to read test file");
-                interpreter.interpret(&contents);
+        if (let Some(input) = matches.get_one::<String>("input")){
+            let path = matches.get_one::<String>("input");
+            if let Some(path_str) = path {
+                let current_dir = env::current_dir().expect("Failed to get current directory");
+                let relative_path = current_dir.join(path_str); // `path_str` is a &String, which implements AsRef<Path>
+    
+                if fs::metadata(&relative_path).is_ok() {
+                    let interpreter =&mut Interpreter::new(); 
+                    let contents = fs::read_to_string(path_str).expect("Failed to read test file");
+                    interpreter.interpret(&contents);
+                } else {
+                    panic!("File not found.");
+                }
             } else {
-                panic!("File not found.");
+                panic!("No input file provided.");
             }
-        } else {
-            panic!("No input file provided.");
-        }
+        } else if (let Some(version) = matches.get_one::<String>("version")){
+            println!("Version 1.0.0")
+        } 
     }
 }
